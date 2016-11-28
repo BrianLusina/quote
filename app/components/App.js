2e79623f7532d31dfa0cc0c6d537803c807cbefd
BrianLusina <lusinabrian@gmail.com>
2016-11-28 22:13:35 +0300

    add fetching quotes from json

--- app/components/App.js
+++ app/components/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component{
         return(
             <div>
               <div>
-                <QuoteContainer apiUrl="api/quotes.json"/>
+                <QuoteContainer apiUrl="../api/quotes.json"/>
                 <ShareQuote />
                 <NewQuote />
               </div>
--- app/components/quote-container.js
+++ app/components/quote-container.js
@@ -5,6 +5,14 @@ export default class QuoteContainer extends React.Component{
 
   constructor(){
     super();
+
+    this.state ={
+      quotes : []
+    }
+  }
+
+  componentWillMount(){
+    this._getQuotes();
   }
 
   render(){
@@ -12,7 +20,7 @@ export default class QuoteContainer extends React.Component{
       <div>
         <div className="quoteText">
             <i className="fa fa-quote-left"> </i>
-            <span id="quote"></span>
+            <span id="quote">{ quotes }</span>
             <i className="fa fa-quote-right"></i>
         </div>
         <div className="quoteAuthor">
@@ -23,19 +31,22 @@ export default class QuoteContainer extends React.Component{
     )
   }
 
+  _getQuotes() {
+    return this.state.quotes.map((quote) => {
+      return quote;
+    });
+  }
+
 //"X-Mashape-Key": "HT2nuAA369mshsFX1vMRe7cfr4drp14TBx9jsnovWg3V75k7o5",
 //url: 'https://kashyap-random_quotes.p.mashape.com/',
 
-  _getQuotes(){
+  _fetchQuotes(){
     jQuery.ajax({
       method:"GET",
-      url: 'https://kashyap-random_quotes.p.mashape.com/',
-      success:function(response){
-        var r = JSON.parse(response);
-        currentQuote = r.quote;
-        currentAuthor = r.author;
+      url: this.props.apiUrl,
+      success:(quotes) => {
+        this.setState({quotes})
       },
-
     });
   }
 }
