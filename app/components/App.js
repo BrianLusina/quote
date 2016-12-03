de2f64a5872bfccc06c50a4e65d48fc61db9f100
BrianLusina <lusinabrian@gmail.com>
2016-12-03 16:37:39 +0300

    Add time interval for quotes

--- app/components/App.js
+++ app/components/App.js
@@ -14,28 +14,32 @@ export default class App extends React.Component{
     }
   }
 
-    render(){
-        return(
+  render(){
+      return(
+          <div>
             <div>
-              <div>
-                <QuoteContainer styles={ this.props.styles } author={ this.state.author } quote={ this.state.quote }/>
-                <ShareQuote styles={ this.props.styles } author={ this.state.author } quote={ this.state.quote }/>
-              </div>
-              <div className={this.props.styles.footer}>
-                  Made with <i className="fa fa-heart"></i> and <i className="fa fa-coffee"></i> by
-              <a href="http://thelusina.netlify.com/">The Lusina</a>
-              </div>
-          </div>
-        );
-    }
+              <QuoteContainer styles={ this.props.styles } author={ this.state.author } quote={ this.state.quote }/>
+              <ShareQuote styles={ this.props.styles } author={ this.state.author } quote={ this.state.quote }/>
+            </div>
+            <div className={this.props.styles.footer}>
+                Made with <i className="fa fa-heart"></i> and <i className="fa fa-coffee"></i> by
+            <a href="http://thelusina.netlify.com/">The Lusina</a>
+            </div>
+        </div>
+      );
+  }
 
     // perform AJAX request right after mounting
     componentDidMount(){
       this._fetchQuotes();
     }
 
+    /**
+    ajax call to getch quotes from server, every 30 seconds.
+    This updates the states of these components which can be passed on to the quote container
+    */
     _fetchQuotes(){
-      setTimeout(function () {
+      setInterval(function () {
         jQuery.ajax({
           method:"GET",
           url:"https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
@@ -57,7 +61,7 @@ export default class App extends React.Component{
           complete: (response, status) => {
           }
         });
-      }.bind(this), 5000);
+      }.bind(this), 30000);
     }
 }
 
