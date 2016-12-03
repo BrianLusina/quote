897547fe8edb2442927f173e0e26c3cc16e0329a
BrianLusina <lusinabrian@gmail.com>
2016-12-03 14:24:35 +0300

    mapped elements to array for easier render

--- app/components/share-quotes.js
+++ app/components/share-quotes.js
@@ -3,73 +3,47 @@ export default class ShareQuote extends React.Component {
         constructor() {
             super();
             this.state = {
-                tweetUrl: "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
-                , tumblrUrl: "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption="
+                tweetUrl: "https://twitter.com/intent/tweet?hashtags=quotes&related=brianlusina&text=",
+                tumblrUrl: "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption="
             }
             this._handleTumblrClick = this._handleTumblrClick.bind(this);
             this._handleTweetClick = this._handleTweetClick.bind(this);
-            this._handleFacebookClick = this._handleFacebookClick.bind(this);
-            this._handleGoogleClick = this._handleGoogleClick.bind(this);
+            this._handleButtonClick = this._handleButtonClick.bind(this);
         }
         componentWillMount() {}
         render() {
-            return ( < div > < a className = {
-                    this.props.styles.socialButton
-                }
-                id = {
-                    this.props.styles.tweetQuote
-                }
-                title = "Tweet this quote!"
-                target = "_blank"
-                onClick = {
-                    this._handleTweetClick
-                } > < i className = "fa fa-twitter fa-2x" > < /i> < /a > < a className = {
-                    this.props.styles.socialButton
-                }
-                id = {
-                    this.props.styles.tumblrQuote
-                }
-                title = "Post this quote on tumblr!"
-                target = "_blank"
-                onClick = {
-                    this._handleTumblrClick
-                } > < i className = "fa fa-tumblr fa-2x" > < /i> < /a > < a className = {
-                    this.props.styles.socialButton
-                }
-                id = {
-                    this.props.styles.facebookQuote
-                }
-                title = "Post this quote on Facebook!"
-                target = "_blank"
-                onClick = {
-                    this._handleFacebookClick
-                } > < i className = "fa fa-facebook fa-2x" > < /i> < /a > < a className = {
-                    this.props.styles.socialButton
-                }
-                id = {
-                    this.props.styles.googlePlusQuote
-                }
-                title = "Post this quote on Google+!"
-                target = "_blank"
-                onClick = {
-                    this._handleGoogleClick
-                } > < i className = "fa fa-google-plus fa-2x" > < /i> < /a > < /div>)
+          const propsObj = this.props;
+
+          var shareButtons = [
+              "fa fa-twitter fa-2x",
+              "fa fa-tumblr fa-2x",
+              "fa fa-facebook fa-2x",
+              "fa fa-google-plus fa-2x"
+          ];
+
+          var list = shareButtons.map((item, indx) => {
+            return <a key = {indx} className= {item} onClick = {this._handleButtonClick}></a>;
+          })
+            return (
+              <div>
+                {list}
+              </div>)
             }
 
-            _openUrl(url) {
-                window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-            }
+          _openUrl(url) {
+              window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+          }
 
-            _inIframe() {
-                    try {
-                        return window.self !== window.top;
-                    }
-                    catch (e) {
-                        console.error("inIframe Error: " + e);
-                        return true;
-                    }
-                }
-                //share via tumblr
+          _inIframe() {
+                try {
+                      return window.self !== window.top;
+                  }
+                  catch (e) {
+                      console.error("inIframe Error: " + e);
+                      return true;
+                  }
+              }
+              //share via tumblr
             _handleTumblrClick(e) {
                     e.preventDefault();
                     if(this._inIframe){
@@ -87,12 +61,12 @@ export default class ShareQuote extends React.Component {
                     }
                 }
                 //share via Facebook
-            _handleFacebookClick(e) {
-                    e.preventDefault();
-                }
                 //share via Google
-            _handleGoogleClick(e) {
-                e.preventDefault();
+
+
+            _handleButtonClick(e){
+              e.preventDefault();
+              console.log(e.target);
             }
         }
         ShareQuote.propTypes = {
