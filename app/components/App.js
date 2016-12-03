f7b065c435ef96f2ca3d18bad4b4086d72091fae
BrianLusina <lusinabrian@gmail.com>
2016-12-03 15:52:30 +0300

    remove other social medial links

--- app/components/App.js
+++ app/components/App.js
@@ -33,33 +33,33 @@ export default class App extends React.Component{
 
     // perform AJAX request right after mounting
     componentDidMount(){
-      this._fetchQuotes();
-    }
+      this._fetchQuotes().bind(this);
 
+    }
     _fetchQuotes(){
-      jQuery.ajax({
-        method:"GET",
-        url:"https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
-        headers: {
-          "X-Mashape-Key": "HT2nuAA369mshsFX1vMRe7cfr4drp14TBx9jsnovWg3V75k7o5",
-          Accept: "application/json",
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        success:(response) => {
-          var json = JSON.parse(response);
-          let author = json["author"];
-          let quote = json['quote'];
-          this.setState({author, quote});
-          console.log("Qoute", this.state.quote," Author: ", this.state.author);
-        },
-        error:(request, errorType, errorMessage) => {
-          console.error(errorType, errorMessage);
-        },
-        timeout: 3000,
-        complete: (response, status) => {
-          console.log(response, status);
-        }
-      });
+      setTimeout(function () {
+        jQuery.ajax({
+          method:"GET",
+          url:"https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
+          headers: {
+            "X-Mashape-Key": "HT2nuAA369mshsFX1vMRe7cfr4drp14TBx9jsnovWg3V75k7o5",
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          success:(response) => {
+            var json = JSON.parse(response);
+            let author = json["author"];
+            let quote = json['quote'];
+            this.setState({author, quote}).bind(this);
+          },
+          error:(request, errorType, errorMessage) => {
+            console.error(errorType, errorMessage);
+          },
+          timeout: 3000,
+          complete: (response, status) => {
+          }
+        });
+      }, 10);
     }
 }
 
--- app/components/new-quote.js
+++ app/components/new-quote.js
@@ -11,7 +11,9 @@ export default class NewQuote extends React.Component{
   render(){
     return(
       <div>
-        <button className={ this.props.styles.button } id={ this.props.styles.newQuote }>New quote</button>
+        <button className={ this.props.styles.button } id={ this.props.styles.newQuote }>
+          New quote
+        </button>
       </div>
     )
   }
