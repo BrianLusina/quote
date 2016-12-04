1439ec3b36f20dce707d91bceedecc7df61a9dbd
BrianLusina <lusinabrian@gmail.com>
2016-12-04 09:23:55 +0300

    re-write webpack prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,56 +1,46 @@
-const path = require('path');
-const webpack = require('webpack');
-const combineLoaders = require('webpack-combine-loaders');
+const path = require('path')
+const webpack = require('webpack')
 
-module.exports ={
-  devtool : "source-map",
-  entry : [
-    __dirname + '/src/index.js'
+module.exports = {
+  devtool: 'source-map',
+
+  entry: [
+    './src/index'
   ],
 
-  output:{
-      filename: 'app.js',
-      path: __dirname + '/build'
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'app.js',
+    publicPath: '/public/'
   },
 
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
-        minimize:true,
-        compress:{
-          warnings:false
-        }
+      minimize: true,
+      compress: {
+        warnings: false
       }
-    ),
+    }),
     new webpack.DefinePlugin({
-      'process.env':{
-        "NODE_ENV":JSON.stringify("production")
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
       }
     })
   ],
 
-  modules:{
-    loader:[
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader:'babel-loader',
-      },
-      {
-          test:/\.css$/,
-          loader:combineLoaders([
-              {
-                  loader: 'style-loader'
-              },
-              {
-                  loader:'css-loader',
-                  query:{
-                      modules:true,
-                      localIdentName: '[name]__local__[hash:base64:5]'
-                  }
-              }
-          ]),
-      },
+  module: {
+    loaders: [
+      { test: /\.js?$/,
+        loader: 'babel',
+        include: path.join(__dirname, 'src') },
+      { test: /\.css?$/,
+        loader: 'style!css!sass',
+        include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.png$/,
+        loader: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'}
     ]
   }
 }
