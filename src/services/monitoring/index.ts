d7acae3c9b885586067bb711f5e60e5930f7f07e
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-06-20 21:02:56 +0300

    chore(monitoring): fix import

--- src/services/monitoring/index.ts
+++ src/services/monitoring/index.ts
@@ -3,9 +3,6 @@ import {
   captureAndLogError,
   captureException,
   captureScope,
-  Levels,
 } from './Monitoring';
 
 export { initializeMonitoring, captureAndLogError, captureException, captureScope };
-
-export type { Levels };
